80d07b63da848339d095d20f4a10cdebf0059d08
Gourav <gourav@Gouravs-MacBook-Air.local>
2025-06-14 19:46:47 +0530

    update

--- src/Pages/User/Footer.jsx
+++ src/Pages/User/Footer.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
+import React from "react";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-gray-900 to-black text-white py-10 px-4 z-20">
+    <footer className="bg-gradient-to-r from-gray-900 to-black text-white py-10 px-4 z-[999]">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center">
         {/* Left Section */}
         <div className="flex flex-col md:flex-row items-center md:items-start">
@@ -15,20 +15,34 @@ function Footer() {
 
         {/* Center Section (Social Links) */}
         <div className="flex space-x-6 mt-6 md:mt-0">
-          <a href="https://github.com/Gourav12321" className="text-gray-400 hover:text-white  " target='blank'>
+          <a
+            href="https://github.com/Gourav12321"
+            className="text-gray-400 hover:text-white  "
+            target="blank"
+          >
             <FaGithub size={25} />
           </a>
-          <a href="https://www.linkedin.com/in/gourav-maurya-1b516a303/" className="text-gray-400 hover:text-white " target='blank'>
+          <a
+            href="https://www.linkedin.com/in/gourav-maurya-1b516a303/"
+            className="text-gray-400 hover:text-white "
+            target="blank"
+          >
             <FaLinkedin size={25} />
           </a>
-          <a href="https://www.instagram.com/gourav_maurya426/" className="text-gray-400 hover:text-white " target='blank'>
+          <a
+            href="https://www.instagram.com/gourav_maurya426/"
+            className="text-gray-400 hover:text-white "
+            target="blank"
+          >
             <FaInstagram size={25} />
           </a>
         </div>
 
         {/* Right Section */}
         <div className="mt-6 md:mt-0 text-gray-400 text-sm">
-          <p>© {new Date().getFullYear()} Gourav Maurya. All Rights Reserved.</p>
+          <p>
+            © {new Date().getFullYear()} Gourav Maurya. All Rights Reserved.
+          </p>
         </div>
       </div>
     </footer>
