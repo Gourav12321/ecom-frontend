62a376fbc4ff65a0916b052fca357360ad91c4e8
Gourav <gourav@Gouravs-MacBook-Air.local>
2025-09-20 14:06:31 +0530

    changes

--- src/Pages/User/Home.jsx
+++ src/Pages/User/Home.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import CategoryList from '../Product/CategoryList'
-import Home2 from '../Product/Home2';
-import BannerProduct from '../Product/BannerProduct';
-import Footer from './Footer';
+import React from "react";
+import CategoryList from "../Product/CategoryList";
+import Home2 from "../Product/Home2";
+import BannerProduct from "../Product/BannerProduct";
+import Footer from "./Footer";
 function Home() {
   return (
-    <div className='flex flex-col gap-5'>
-      <div className='w-full'>
-      <CategoryList/>
+    <div className="flex flex-col gap-5 relative min-h-screen">
+      <div className="w-full">
+        <CategoryList />
       </div>
-      <BannerProduct/>
-      <div className='w-full flex flex-col gap-5'>
-        <Home2/>
+      <BannerProduct />
+      <div className="w-full flex flex-col gap-5">
+        <Home2 />
       </div>
       {/* <Footer/> */}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
